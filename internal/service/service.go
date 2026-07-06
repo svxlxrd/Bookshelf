95e88a5837c9b2e3a3a107d2d34f8b2470cf14a2
Михаил <mishakhegai@gmail.com>
2026-07-07 01:20:28 +0300

    feat(service): complete service layer

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,13 +1,29 @@
 package service
 
-// import "github.com/bookshelf/monolith/internal/repository"
+import "github.com/bookshelf/monolith/internal/repository"
 
 type Service struct {
-	User UserService
-	// Book
-	// Review
+	User   UserService
+	Book   BookService
+	Review ReviewService
 }
 
-// func New(repos *repository.Repository, jwtSecret string) *Service {
+func New(repos *repository.Repository, jwtSecret string) *Service {
+	return &Service{
+		User: UserService{
+			repo:      repos.User,
+			jwtSecret: jwtSecret,
+		},
 
-// }
+		Book: BookService{
+			bookRepo: repos.Book,
+			userRepo: repos.User,
+		},
+
+		Review: ReviewService{
+			reviewRepo: repos.Review,
+			bookRepo:   repos.Book,
+			userRepo:   repos.User,
+		},
+	}
+}
